9b1f82024cdddf92ca321ea523ce94544088dab3
Todd Kaufmann <toddkaufmann+g@gmail.com>
2018-04-17 15:55:26 -0400

    add SUBJECT, and describe the fields

--- register-example.bat
+++ register-example.bat
@@ -20,9 +20,19 @@ set email=admin@example.com
 @rem req:  your domain to get cert form
 set domain=example.com
 
+@rem CN: CommonName - your domain
+@rem OU: OrganizationalUnit
+@rem O: Organization
+@rem L: Locality (City, usually)
+@rem ST: StateOrProvinceName
+@rem C: CountryName
+set SUBJECT="/C=US/ST=Pennsylvania/L=Pittsburgh/O=Company Ltd.,/OU=web marketing/CN=%domain%/emailAddress=%email%" 
+
 @rem req:   a folder that can serve static files
+@rem 
 set tc_static=C:\apache-tomcat-7.0.81\webapps\static
 
-set well_known_acme=%tc_static\.well-known\acme-challenge
+@rem It will need to be able to serve up static files for challenge from:
+@rem set well_known_acme=%tc_static\.well-known\acme-challenge
 
 call pjac-gen.bat
