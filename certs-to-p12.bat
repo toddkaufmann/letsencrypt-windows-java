d13eebfcaa259d0b2d7b7ecd3518aac915927908
Todd Kaufmann <toddkaufmann+g@gmail.com>
2018-04-17 16:13:10 -0400

    Remove extra steps for keystore (heytool commands, etc).  Not tested / not needed.

--- certs-to-p12.bat
+++ certs-to-p12.bat
@@ -41,36 +41,7 @@ if exist %cert_dir%\cert-and-key.p12 (
 
 exit /b %errorlevel%
 
-@rem this failed -- args don't look right (eg '-chain' ?)
-if exist %cert_dir%\cert.p12 (
-    @echo Converted to .p12 already
-) else (
-    openssl pkcs12 -export -in %cert_dir%\cert.pem -inkey %dom_priv_key% ^
-                      -out %cert_dir%\cert.p12 ^
-		      -name tomcat -chain -CAFile %cert_dir%\fullchain.pem
-    echo 1. errorlevel is %errorlevel%g
-    %pause%
-)
+@rem there were more steps here for creating and importing into jks,
+@rem see an old revision, or one of the many posts describing it.
 
-exit /b %errorlevel%
-
-pause
-
-@rem 2 import w/ keytool
-
-keytool -importkeystore -deststorepass <a_password_for_your_java_keystore> -destkeypass <a_password_for_the_key_in_the_keystore>-destkeystore tomcat.keystore -srckeystore <exported_private_key_and_cert.p12> -srcstoretype PKCS12 -srcstorepass <the_password_I_told_you_to_remember> -alias tomcat
-
-
-@rem 3 intermediate keys?  or is fullchain good ?
-
-keytool -import -alias cross -keystore tomcat.keystore -trustcacerts -file gd_cross_intermediate.crt
-keytool -import -alias intermed -keystore tomcat.keystore -trustcacerts -file gd_intermediate.crt
-
-@rem 4 move keystore to tomcat
-@rem  .. copy somewhere, maybe in to conf/ 
-
-@rem 5 configure tomcat
-
-<Connector port="8443" maxThreads="200" scheme="https" secure="true" SSLEnabled="true" keystoreFile="path_to_your_keystore_file" keystorePass="the_password_you_created_for_your_keystore" clientAuth="false" sslProtocol="TLS"/>
-
-@rem 6 restart, test tomcat
+@rem I think just using .p12 is easier.
