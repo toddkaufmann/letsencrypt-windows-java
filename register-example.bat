7340d662189e952361b814766faaa7fd7c6ad3c5
Todd Kaufmann <toddkaufmann+g@gmail.com>
2019-04-03 22:44:55 -0400

    show setting the PATH explicitly as well.

--- register-example.bat
+++ register-example.bat
@@ -1,5 +1,16 @@
-@rem TODO:
-@rem PATH:  java 8, openssl, jar
+@rem PATH should have:  java 8, openssl, jar
+
+@rem They don't necessarily need to be in the global system environment
+
+@rem sample settings:
+set SSLDIR=D:\ssl-support
+set PATH=%SSLDIR%\openssl-1.1.0h;%SSLDIR%\jre1.8.0_172\bin;%PATH%
+set JRE_HOME=%SSLDIR%\jre1.8.0_172
+
+@rem where all the work takes place:
+@rem  easier (necessary?) if everything is in same drive-letter
+@rem  (and be sure to call from there)
+set LE=D:%SSLDIR%\letsencrypt
 
 @rem Configure variables here,
 @rem then call main script
@@ -8,18 +19,17 @@
 @rem ROOT of all your stuff;
 
 @rem letsencrypt folder
-set LE=\Users\todd\LE
+@rem set LE=\Users\todd\LE  -- see above
 @rem it should exist
 
 @rem also:  logdir workdir certdir
 set logdir=%LE%\log
 
 @rem TODO folder should be different for each? .. is easiest.
-@rem can you share acct_key ?
 
-@rem req:
+@rem Required:
 set email=admin@example.com
-@rem req:  your domain to get cert form
+@rem Required:  your domain to get cert form
 set domain=example.com
 
 @rem CN: CommonName - your domain
@@ -31,8 +41,9 @@ set domain=example.com
 set SUBJECT="/C=US/ST=Pennsylvania/L=Pittsburgh/O=Company Ltd.,/OU=web marketing/CN=%domain%/emailAddress=%email%" 
 
 @rem req:   a folder that can serve static files
-@rem 
-set tc_static=C:\apache-tomcat-7.0.81\webapps\static
+@rem ROOT is maybe okay too
+@rem tomcat9 has been tested and works too.
+set tc_static=D:\apache-tomcat-7.0.81\webapps\static
 
 @rem It will need to be able to serve up static files for challenge from:
 @rem set well_known_acme=%tc_static\.well-known\acme-challenge
